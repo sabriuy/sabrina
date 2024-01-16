00ff34d8951926269e966c8eba953e7a9a997918
Sabrina <sabrinaalejandra1992@gmail.com>
2024-01-16 14:59:03 -0300

    Delete proyecto barraca-alianza/js/sliderbarraca.js

--- proyecto barraca-alianza/js/sliderbarraca.js
+++
@@ -1,5 +0,0 @@
-
-document.getElementsByClassName('.toggle').addEventListener('click', function() {
-    document.getElementsByClassName('.navbar-categorias').classList.toggle('.navbar-categorias_visible');
-})
-
