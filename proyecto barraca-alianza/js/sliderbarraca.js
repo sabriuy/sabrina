71860743d506b23111079ba3948c5b07fe4d611d
Sabrina <sabrinaalejandra1992@gmail.com>
2024-01-16 14:53:00 -0300

    Add files via upload

---
+++ proyecto barraca-alianza/js/sliderbarraca.js
@@ -0,0 +1,5 @@
+
+document.getElementsByClassName('.toggle').addEventListener('click', function() {
+    document.getElementsByClassName('.navbar-categorias').classList.toggle('.navbar-categorias_visible');
+})
+
